81748b80ae3852bf95b8b9610226678620df68a5
hankthemason <42900752+hankthemason@users.noreply.github.com>
2021-06-02 12:41:21 -0400

    added styles for transactions module

---
+++ client/src/modules/transactions/styles/index.ts
@@ -0,0 +1,24 @@
+const DateSelect = {
+  width: "7rem",
+  margin: ".25rem"
+}
+
+const TransactionsTable: TransactionsTable = {
+  category: {
+    width: "12rem"
+  },
+  amount: {
+    width: "8rem"
+  },
+  notes: {
+    width: "18rem"
+  },
+  date: {
+    width: "12rem"
+  }
+}
+
+export const styles = {
+  DateSelect: DateSelect,
+  TransactionsTable: TransactionsTable
+}
